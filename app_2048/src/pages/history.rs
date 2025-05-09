05a51e5d6394a6f79bea197a899215225f769cef
Bailey Townsend <baileytownsend2323@gmail.com>
2025-05-09 15:48:17 -0500

    about to rename some stuff

--- app_2048/src/pages/history.rs
+++ app_2048/src/pages/history.rs
@@ -1,12 +1,36 @@
-use implicit_clone::unsync::*;
-use wasm_bindgen::{JsCast, UnwrapThrowExt};
-use web_sys::{HtmlInputElement, KeyboardEvent};
+use std::rc::Rc;
+use types_2048::blue;
 use yew::prelude::*;
-
-pub enum HistoryAction {}
 
 struct HistoryState {
     games: Vec<types_2048::blue::_2048::game::RecordData>,
+}
+
+pub enum HistoryAction {
+    GetLocalGames,
+}
+
+impl Reducible for HistoryState {
+    /// Reducer Action Type
+    type Action = HistoryAction;
+
+    /// Reducer Function
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        let games: Vec<blue::_2048::game::RecordData> = match action {
+            HistoryAction::GetLocalGames => {
+                // let db = match Database::open(DB_NAME).await {
+                //     Ok(db) => db,
+                //     Err(err) => {
+                //         log::error!("{}", err);
+                //         vec![]
+                //     }
+                // };
+                vec![]
+            }
+        };
+
+        Self { games }.into()
+    }
 }
 
 #[function_component(HistoryPage)]
@@ -29,8 +53,29 @@ pub fn history() -> Html {
     // };
 
     html! {
-        <>
-            <h1>{ "History" }</h1>
-        </>
+        <div class="p-4">
+            <div class="max-w-4xl mx-auto space-y-4 justify-center">
+                <div
+                    class=" bg-base-100 relative flex min-h-[6rem] max-w-4xl min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden bg-cover bg-top p-4 xl:py-10 "
+                >
+                    <div role="tablist" class="tabs tabs-lift">
+                        <a role="tab" class="tab">{ "Local" }</a>
+                        <a role="tab" class="tab tab-active">{ "Remote" }</a>
+                        <a role="tab" class="tab">{ "Both" }</a>
+                    </div>
+                </div>
+                // Header
+                // <div class="card bg-base-100 shadow-xl">
+                //     <div class="card-body">
+                //         <h2 class="card-title text-3xl font-bold">{ "Your at://2048 Stats" }</h2>
+                //         <p class="text-base-content/70">
+                //             { "Track your progress and achievements" }
+                //         </p>
+                //     </div>
+                // </div>
+                // Main Stats Grid
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" />
+            </div>
+        </div>
     }
 }
